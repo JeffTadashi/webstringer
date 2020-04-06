06a790bc92bb8293d99adfb9f4164cdb9215c408
jefftadashi <jeff@jefftadashi.com>
2020-04-06 15:51:15 -0500

    added many features

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Exmaple docker:
 ```
 (create webstringer subdirectory with apikey)
 docker run -it --rm -v $(pwd)/webstringer/:/vol/ jefftadashi/webstringer
+docker run -d --restart unless-stopped -v $(pwd)/webstringer/:/vol/ jefftadashi/webstringer
 
 (for building on my master machine)
-docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 -t jefftadashi/webstringer:latest -t jefftadashi/webstringer:multi-arch --push .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 -t jefftadashi/webstringer:latest -t jefftadashi/webstringer:multi-arch --push .
 ```
--- webstringer.py
+++ webstringer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # TODO: program in return address, change default to something not jefftadashi.com related
-# TODO: Change jam script to docker format
 
 import requests
 import json
@@ -13,12 +12,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--url", required=True, help="URL of the site to monitor")
-parser.add_argument("-m", "--match",  help="String of text to match")
-parser.add_argument("-n", "--nomatch",  help="String of text to NOT match")
-parser.add_argument("-a", "--api",  required=True, help="File location of API key. Should be a single line text file with the key in first line.")
-parser.add_argument("-e", "--email",  action='append', required=True, help="Email address to send alert to. Repeat argument for multiple email addresses")
+parser.add_argument("-m", "--match", action='append', help="String of text to match. Repeat argument call for multiple 'AND' type logic")
+parser.add_argument("-n", "--nomatch", action='append', help="String of text to NOT match. Repeat argument call for multiple 'AND' type logic")
+parser.add_argument("-a", "--api", help="File location of API key. Should be a single line text file with the key in first line.")
+parser.add_argument("-e", "--email", action='append', help="Email address to send alert to. Repeat argument call for multiple email addresses")
+parser.add_argument("-t", "--testmode", action='store_true', help="Test matching mode: Emails are NOT sent, and script ends after it finds it's first match.")
 # parser.add_argument("-f", "--from-email",  action='append', help="Email address to send from. Default is website-checker@jefftadashi.com")
-
 args = parser.parse_args()
 
 # Exit, if user did not specify match or nomatch
@@ -43,39 +42,47 @@ headers = {
 try:
     with open(args.api) as file:
         read_api_key = file.readline().strip()
-except IOError:
-    print("API Key file not accessible! Please be sure we have permissions to read it, and it's spelled correctly.")
-    sys.exit(1)
+except:
+    if args.testmode == False: #only error exit if not in testmode. In testmode, API key not needed.
+        print("API Key file not accessible, or was not defined! Please be sure we have permissions to read it, and it's spelled correctly. Use argument -a or --api")
+        sys.exit(1)
 
 
 # Create email address list in proper format (single-entry dictionaries inside a list)
+# Exit if no emails were defined. Bypass if in testmode
 final_email_list=[]
-for x in args.email: 
-    final_email_list.append({"email":x})
+if args.email is None:
+    if args.testmode == False:
+        print("No destination email addresses defined! Use -e or --email argument")
+        sys.exit(1)
+else:
+    for x in args.email: 
+        final_email_list.append({"email":x})
 
 
 
-# Send initial test/starting email
-print ("Sending initial test/notice email...")
-sg = sendgrid.SendGridAPIClient(api_key=read_api_key)
-init_data = {
-"personalizations": [
-    {
-   "to": final_email_list,
-   "subject": "JeffTadashi/Webstringer: Begin monitoring notice"
+# Send initial test/starting email, unless in testmode
+if args.testmode == False:
+    print ("Sending initial test/notice email...")
+    sg = sendgrid.SendGridAPIClient(api_key=read_api_key)
+    init_data = {
+    "personalizations": [
+        {
+    "to": final_email_list,
+    "subject": "JeffTadashi/Webstringer: Begin monitoring notice"
+        }
+    ],
+    "from": {
+        "email": "website-checker@jefftadashi.com"
+    },
+    "content": [
+        {
+        "type": "text/plain",
+        "value": "Hello! This is a notice message to make sure your email is working, and to let you know that monitoring has begun on the following URL: " + args.url
+        }
+    ]
     }
-],
-"from": {
-    "email": "website-checker@jefftadashi.com"
-},
-"content": [
-    {
-    "type": "text/plain",
-    "value": "Hello! This is a notice message to make sure your email is working, and to let you know that monitoring has begun on the following URL: " + args.url
-    }
-]
-}
-response = sg.client.mail.send.post(request_body=init_data)
+    response = sg.client.mail.send.post(request_body=init_data)
 
 
 
@@ -86,28 +93,36 @@ while True:
     r = requests.get(args.url, headers=headers)
     #print(r.text)
 
-    match = False
-    nomatch = False
+    final_match = False
+    final_nomatch = False
 
     # Perform logic if the match or nomatch strings were defined, and if they did/didn't exist in the web request
-    if args.match is None:
-        match = True 
-    elif args.match in r.text:
-        match = True 
+    if args.match is None: #if argument was never defined, then set to pass
+        final_match = True 
+    else: 
+        final_match = True # set true now, then cycle thru "match" and change to false if match NOT found
+        for i in args.match: 
+            if i not in r.text:
+                final_match = False
 
-    if args.nomatch is None:
-        nomatch = True 
-    elif args.nomatch not in r.text:
-        nomatch = True 
+    if args.nomatch is None: #if argument was never defined, then set to pass
+        final_nomatch = True 
+    else: 
+        final_nomatch = True # set true now, then cycle thru "nomatch" and change to false if match found
+        for i in args.nomatch: 
+            if i in r.text:
+                final_nomatch = False
+   
+        
 
 
-
-    if match == True and nomatch == True:
+    if final_match == True and final_nomatch == True:
         print("Trigger is fully matched!! Sending email, then waiting 2 hours until next check.")
 
-        # TEMP TESTING/DEBUG ONLY
-        #print("temp exit")
-        #sys.exit(0)
+        if args.testmode == True:
+            # End script, without sending email, if using testmode
+            print("Test mode completing, Exiting script...")
+            sys.exit(0)
 
 
 
@@ -137,7 +152,7 @@ while True:
 
         # Delay for 2 hours, then check again.
         time.sleep(7200)
-        #sys.exit(0)
+    
 
     # repeat this loop every 60 seconds
     time.sleep(60)
